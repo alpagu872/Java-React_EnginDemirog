04d64ae7d5ac3ff17238dec652d7fe62b2a8093e
Alpagu E <alpagu872@gmail.com>
2022-05-08 09:13:15 +0300

    Tüm Ödevler ve Alıştırmalar Tamamlandı. || 9:13 AM

---
+++ src/IlkDersOdevler/KalinVeInceHarfler/Main.java
@@ -0,0 +1,20 @@
+package IlkDersOdevler.KalinVeInceHarfler;
+
+public class Main {
+    public static void main(String[] args) {
+        String harf = "i";
+
+        switch (harf.toUpperCase()){
+            case "I","A","U","O":
+                System.out.println( harf + " İnce Harf");
+                break;
+            case "İ","E","Ü","Ö":
+                System.out.println( harf + " Kalın Harf");
+                break;
+            default:
+                System.out.println("Normal");
+
+        }
+
+    }
+}
