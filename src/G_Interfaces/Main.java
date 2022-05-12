7616f70f5a77dadd0548608362efc02e65a2e3df
Alpagu E <alpagu872@gmail.com>
2022-05-12 22:36:22 +0300

    4.Video izlendi. || 10:36 PM

--- src/G_Interfaces/Main.java
+++ src/G_Interfaces/Main.java
@@ -2,6 +2,13 @@ package G_Interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Logger[] loggers = new Logger[]{new DatabaseLogger(),new EmailLogger(),new SmsLogger(), new FileLogger()};
+
+
+        CustomerManager customerManager = new CustomerManager(loggers);
+
+
+        Customer hasanAkgun = new Customer(1, "Hasan", "Akgün");
+        customerManager.add(hasanAkgun);
     }
 }
